a3e3bca35f859860d0c7dcb4a9e2328317c75c0a
Assim <assim.s@techversantinfo.com>
2023-01-11 17:58:02 +0530

    added test func

---
+++ db/sqlc/store_test.go
@@ -0,0 +1,76 @@
+package db
+
+import (
+	"context"
+	"testing"
+
+	"github.com/Assim-sainulabdeen/simplebank/utils"
+	"github.com/stretchr/testify/require"
+)
+
+func TestTransferTx(t *testing.T) {
+	store := NewStore(testDB)
+
+	account1 := CreateRandomAccount(t)
+	account2 := CreateRandomAccount(t)
+
+	err := make(chan error)
+	results := make(chan TransferTxResult)
+
+	n := 5
+	amount := utils.RandomMoney()
+
+	for i := 0; i < n; i++ {
+		go func() {
+			result, trErr := store.TransferTx(context.Background(), TransferTxParams{
+				FromAccountId: account1.ID,
+				ToAccountId:   account2.ID,
+				Amount:        amount,
+			})
+			err <- trErr
+			results <- result
+
+		}()
+	}
+
+	for i := 0; i < n; i++ {
+		err := <-err
+		require.NoError(t, err)
+
+		result := <-results
+		require.NotEmpty(t, result)
+
+		transfer := result.Transfer
+		require.NotEmpty(t, transfer)
+
+		require.Equal(t, account1.ID, transfer.FromAccountID)
+		require.Equal(t, account2.ID, transfer.ToAccountID)
+		require.Equal(t, amount, transfer.Amount)
+		require.NotZero(t, transfer.ID)
+		require.NotZero(t, transfer.CreatedAt)
+
+		_, err = store.GetTransfer(context.Background(), transfer.ID)
+		require.NoError(t, err)
+
+		fromEntry := result.FromEntry
+		require.NotEmpty(t, fromEntry)
+		require.Equal(t, account1.ID, fromEntry.AccountID)
+		require.Equal(t, -amount, fromEntry.Amount)
+		require.NotZero(t, fromEntry.ID)
+		require.NotZero(t, fromEntry.CreatedAt)
+
+		_, err = store.GetEntry(context.Background(), fromEntry.ID)
+		require.NoError(t, err)
+
+		toEntry := result.ToEntry
+		require.NotEmpty(t, toEntry)
+		require.Equal(t, account2.ID, toEntry.AccountID)
+		require.Equal(t, amount, toEntry.Amount)
+		require.NotZero(t, toEntry.ID)
+		require.NotZero(t, toEntry.CreatedAt)
+
+		_, err = store.GetEntry(context.Background(), toEntry.ID)
+		require.NoError(t, err)
+
+	}
+}
